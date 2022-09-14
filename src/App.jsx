3a000642b2ced40b73ceee3eccf8be2f48ce646c
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-09-14 23:18:46 +0000

    Oragnized assets/icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function App() {
   return (
     <div className="h-screen w-full bg-[#212121]">
---
+++ src/assets/index.js
@@ -0,0 +1,55 @@
+import arrow_up from "./icons/arrow_up.png";
+import arrow_down from "./icons/arrow_down.png";
+import arrow_left from "./icons/arrow_left.png";
+import arrow_right from "./icons/arrow_right.png";
+import bell from "./icons/bell.png";
+import bookmark from "./icons/bookmark.png";
+import calendar from "./icons/calendar.png";
+import chart from "./icons/chart.png";
+import chat from "./icons/chat.png";
+import check_ring from "./icons/check_ring.png";
+import clock from "./icons/clock.png";
+import compass from "./icons/compass.png";
+import desk from "./icons/desk.png";
+import eye from "./icons/eye.png";
+import favorite from "./icons/favorite.png";
+import file from "./icons/file.png";
+import flag from "./icons/flag.png";
+import folder from "./icons/folder.png";
+import home from "./icons/home.png";
+import minus from "./icons/minus.png";
+import plus from "./icons/plus.png";
+import settings from "./icons/settings.png";
+import sound_max from "./icons/sound_max.png";
+import toggle_off from "./icons/toggle_off.png";
+import toggle_on from "./icons/toggle_on.png";
+import user from "./icons/user.png";
+
+export {
+  arrow_down,
+  arrow_left,
+  arrow_right,
+  arrow_up,
+  bell,
+  bookmark,
+  calendar,
+  chart,
+  chat,
+  check_ring,
+  clock,
+  compass,
+  desk,
+  eye,
+  favorite,
+  file,
+  flag,
+  folder,
+  home,
+  minus,
+  plus,
+  settings,
+  sound_max,
+  toggle_off,
+  toggle_on,
+  user,
+};
