5a2632753993c5abb12b2b42ad62a975acc1baa6
devsammyy <yinkaadebayo31@gmail.com>
2023-01-27 13:59:54 +0100

    Create Modal to alert user

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ text, showButton }) => {
   return (
     <>
       <div className="justify-center items-center backdrop-filter backdrop-blur-xl flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto w-[60%] mx-auto max-w-3xl">
+        <div className="relative w-auto w-[80%] mx-auto max-w-3xl">
           {/*content*/}
           <div className="border-0 rounded-lg  shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
