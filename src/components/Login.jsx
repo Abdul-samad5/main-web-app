99b0b025b71540b4cbd935557e95e7c2208ccda7
Omole Daniel <omoledaniel13@gmail.com>
2023-02-10 10:06:11 +0100

    added logo to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import { UserContext } from "../context/UserContext";
 import { styles } from "../constants";
 import { userLogin } from "../services/services";
 import Modal from "./Modal";
+import { logo_2 } from "../assets";
 
 const Login = ({ handleClick }) => {
   // Initialize state for the login to enable user login
@@ -96,88 +97,95 @@ const Login = ({ handleClick }) => {
   }
 
   return (
-    <div className="max-w-[400px] w-full mx-auto">
-      <h1 className="text-center text-[28px] mb-[40px] font-normal">
-        Login to your account
-      </h1>
-      <form className="w-full" onSubmit={formSubmit}>
-        <div className="mb-4 w-full">
-          <label htmlFor="email" className="w-full mb-3 ml-2">
-            Email address
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.value}
-            placeholder="Enter your email address"
-            onChange={handleChange}
-            className="w-full border border-brand-stroke rounded-lg p-3"
-          />
-        </div>
-        <div className="w-full mb-4">
-          <label htmlFor="password" className="w-full mb-3 ml-2">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            value={formData.value}
-            placeholder="Enter password"
-            onChange={handleChange}
-            className="w-full border border-brand-stroke rounded-lg p-3"
-          />
-        </div>
-        <div className="w-full mb-4 flex gap-2 items-center">
-          <input
-            type="checkbox"
-            name="remember"
-            id="remember"
-            checked={formData.remember}
-            onChange={handleChange}
-          />
-          <label htmlFor="remember">Remember me</label>
-        </div>
-        {message.text && (
-          <p className={`${message.color} text-sm text-center`}>
-            {message?.text}
-          </p>
-        )}
-        <button
-          className={`${styles.button}`}
-          disabled={loading ? true : false}
-        >
-          {loading && (
-            <svg className="animate-spin spin h-5 w-5 mr-3" viewBox="0 0 50 50">
-              <circle
-                className="path"
-                cx="25"
-                cy="25"
-                r="20"
-                fill="none"
-                strokeWidth="5"
-              ></circle>
-            </svg>
+    <div className="w-full px-[15px] lg:w-[80%] mx-auto relative pt-[80px] ">
+      <div className="py-5 absolute top-0 sm[320px]:hidden left-5 lg:-left-[100px]">
+          <a href="/" className="w-[50px] block">
+            <img src={logo_2} alt="Yetti Logo" />
+          </a>
+      </div>
+      <div className="max-w-[400px] w-full mx-auto">
+        <h1 className="text-center text-[28px] mb-[40px] font-normal">
+          Login to your account
+        </h1>
+        <form className="w-full" onSubmit={formSubmit}>
+          <div className="mb-4 w-full">
+            <label htmlFor="email" className="w-full mb-3 ml-2">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.value}
+              placeholder="Enter your email address"
+              onChange={handleChange}
+              className="w-full border border-brand-stroke rounded-lg p-3"
+            />
+          </div>
+          <div className="w-full mb-4">
+            <label htmlFor="password" className="w-full mb-3 ml-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.value}
+              placeholder="Enter password"
+              onChange={handleChange}
+              className="w-full border border-brand-stroke rounded-lg p-3"
+            />
+          </div>
+          <div className="w-full mb-4 flex gap-2 items-center">
+            <input
+              type="checkbox"
+              name="remember"
+              id="remember"
+              checked={formData.remember}
+              onChange={handleChange}
+            />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          {message.text && (
+            <p className={`${message.color} text-sm text-center`}>
+              {message?.text}
+            </p>
           )}
-          {loading ? "Processing..." : "Login to account"}
-        </button>
-        <div className="w-full flex justify-between items-center mt-2">
           <button
-            type="button"
-            className="text-brand-gray font-normal text-[14px]"
-            onClick={() => handleClick("forgotPassword")}
+            className={`${styles.button}`}
+            disabled={loading ? true : false}
           >
-            Forgot password?
+            {loading && (
+              <svg className="animate-spin spin h-5 w-5 mr-3" viewBox="0 0 50 50">
+                <circle
+                  className="path"
+                  cx="25"
+                  cy="25"
+                  r="20"
+                  fill="none"
+                  strokeWidth="5"
+                ></circle>
+              </svg>
+            )}
+            {loading ? "Processing..." : "Login to account"}
           </button>
-          <Link to="/register">
+          <div className="w-full flex justify-between items-center mt-2">
             <button
+              type="button"
               className="text-brand-gray font-normal text-[14px]"
-              type="button">
-              Register
+              onClick={() => handleClick("forgotPassword")}
+            >
+              Forgot password?
             </button>
-          </Link>
-        </div>
-      </form>
-      {showModal && <Modal text={modalContent} showButton={showButton} />}
+            <Link to="/register">
+              <button
+                className="text-brand-gray font-normal text-[14px]"
+                type="button">
+                Register
+              </button>
+            </Link>
+          </div>
+        </form>
+        {showModal && <Modal text={modalContent} showButton={showButton} />}
+      </div>
     </div>
   );
 };
