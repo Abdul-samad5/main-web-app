ceb1d05fab98d791d4fb630eb623086048928cc2
Omole Daniel <omoledaniel13@gmail.com>
2023-04-04 15:08:57 +0100

    fixed line error

--- src/components/UserAccount.jsx
+++ src/components/UserAccount.jsx
@@ -15,21 +15,21 @@ const UserAccount = () => {
             <p className="text-2xl text-black-800 font-bold w-full mx-auto mb-1">User Account</p>
             
             <div className='w-full rounded-lg shadow-lg h-auto mt-3'>
-                <div className='border-b-2 py-7 border-brand-primary h-20 w-full px-6 lg:px-10'>
-                    <div className='flex w-full lg:w-1/2 px-2'>
+                <div className='border-b-2 py-7 border-brand-primary h-20 w-full px-4 lg:px-10'>
+                    <div className='flex w-full justity-between lg:w-1/2 md:w-2/3'>
                         <p 
                             onClick={() => handleClick(0)}
-                            className={currentPage === 0 ? 'opacity-50 border-t-2 px-6 py-3 hover:cursor-pointer border-l-2 border-b-white border-brand-primary border-r-2' : "px-5 py-3 hover:cursor-pointer opacity-50"}>
+                            className={currentPage === 0 ? 'opacity-50 border-t-2 px-2 lg:px-6 py-3 text-sm lg:text-base hover:cursor-pointer border-l-2 border-b-white border-brand-primary border-r-2' : "lg:px-5 text-xs px-1 lg:text-lg py-3 hover:cursor-pointer opacity-50"}>
                             Profile
                         </p>
                         <p 
                             onClick={() => handleClick(1)}
-                            className={currentPage === 1 ? 'opacity-50 border-t-2 px-6 py-3 border-l-2 border-b-white hover:cursor-pointer border-brand-primary border-r-2' : "px-5 hover:cursor-pointer py-3 opacity-50"}>
+                            className={currentPage === 1 ? 'opacity-50 border-t-2 px-2 lg:px-6 py-3 text-sm lg:text-base border-l-2 border-b-white hover:cursor-pointer border-brand-primary border-r-2' : "lg:px-5 px-1 text-xs lg:text-lg hover:cursor-pointer py-3 opacity-50"}>
                             Security
                         </p>
                         <p 
                             onClick={() => handleClick(2)}
-                            className={currentPage === 2 ? 'opacity-50 border-t-2 px-6 py-3 border-l-2 border-b-white hover:cursor-pointer border-brand-primary border-r-2' : "px-5 hover:cursor-pointer py-3 opacity-50"}>
+                            className={currentPage === 2 ? 'opacity-50 border-t-2 px-2 lg:px-6 py-3 text-sm lg:text-base border-l-2 border-b-white hover:cursor-pointer border-brand-primary border-r-2' : "text-xs px-1 lg:text-lg lg:px-5 hover:cursor-pointer py-3 opacity-50"}>
                             Bank Information
                         </p>
                     </div>
