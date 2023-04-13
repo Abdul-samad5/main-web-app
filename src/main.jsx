e3fe28cb8e843f7322f7d74c28bb1a555c41dbae
techlhord <yinkaadebayo31@gmail.com>
2023-04-13 20:51:32 +0100

    trying to fix PWA isses

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 import UserContext from "./context/UserContext";
 import CartContextProvider from "./context/CartContext";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -18,18 +19,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/serviceWorker.js").then(
-      function (registration) {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
+serviceWorker.register();
