3b809298048e3d9c524a4ec78d59afe973698bec
AyodejiOmole <omoledaniel13@gmail.com>
2023-10-18 16:52:08 +0100

    finished styling of resetpasswordconfirm page

--- src/components/ResetPasswordConfirm.jsx
+++ src/components/ResetPasswordConfirm.jsx
@@ -1,28 +1,22 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styles } from '../constants';
-import { userLogin } from '../services/services';
-import { UserContext } from '../context/UserContext';
 import Modal from './Modal';
-import Cookies from 'js-cookie';
 import axios from 'axios';
 import { logo_2 } from '../assets';
 import { Link, useParams } from 'react-router-dom';
+import { BASE_URL } from '../services/services';
 
-const ResetPasswordConfirm = ({ handleClick }) => {
+const ResetPasswordConfirm = ( ) => {
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const [modalText, setModalText] = useState(null);
-  const tk = Cookies.get("_tksr");
   const { id, token } = useParams();
 
   const [formData, setFormData] = useState({
-    email: '',
     password: '',
-    remember: false,
+    rePassword: false,
   });
-
-  const { onUserLogin } = useContext(UserContext);
 
   const navigate = useNavigate();
 
@@ -40,68 +34,33 @@ const ResetPasswordConfirm = ({ handleClick }) => {
     e.preventDefault();
 
     let user = {
-      email: formData.email,
       password: formData.password,
     };
     
     setLoading(true);
+
     try {
-      const response = await userLogin(user);
-      if (!response.statusText === 'OK') return;
-
-      if (response) {
-        const token = response.data.data.access;
-        const email = response.data.data.user.email;
-        const user_id = response.data.data.user.user_id;
-        const emailUrl = response.data.data.user.user_email_url;
-        const type = response.data.data.type;
-        const emailVerify = response.data.data.user.user_is_active;
-
-        console.log(emailVerify);
-        console.log(type);
-        console.log(response);
-
-        onUserLogin(token, email, user_id, emailUrl, emailVerify, type);
-
-        if (response.data.data.user.has_store === false) {
-          setLoading(false);
-          setShowModal(true);
-          setModalText('Login Success! Please create a store to continue');
-          setTimeout(() => {
+        const res = await axios.post(`${BASE_URL}auth/reset-password-confirm/${id}/${token}`,
+            user
+        );
+        console.log(res);
+        if (!response.statusText === 'OK') return;
+        
+        setShowModal(true);
+        setModalText('Your password has been changes succesfully!');
+        setTimeout(() => {
             setShowModal(false);
-            handleClick('createStore');
-          }, 3000);
-          navigate('/dashboard');
-        } else {
-          setLoading(false);
-          setShowModal(true);
-          setModalText('Login Success! Please wait...');
-          setTimeout(() => {
-            setShowModal(false);
-
-            navigate('/dashboard');
-          }, 3000);
-        }
-      }
+            navigate('/login');
+        }, 3000);
     } catch (err) {
-      setLoading(false);
-      if (err.response.data.email && err.response.data.password) {
+        // setShowModal(true);
+        // setModalText('Login Success! Please create a store to continue');
+        // setTimeout(() => {
+        //     setShowModal(false);
+        // }, 3000);
+        console.log(err);
+    } finally {
         setLoading(false);
-        setShowModal(true);
-        setModalText('Field(s) cannot be empty!');
-      } else if (err.code === 'ERR_NETWORK') {
-        setLoading(false);
-        setShowModal(true);
-        setModalText('There might be a problem with your network connection!');
-      } else if (err.response.status === 401) {
-        setLoading(false);
-        setShowModal(true);
-        setModalText('Invalid login credentials');
-      }
-      setTimeout(() => {
-        setShowModal(false);
-      }, 3000);
-      console.log(err);
     }
   }
 
@@ -116,22 +75,9 @@ const ResetPasswordConfirm = ({ handleClick }) => {
       <div className='max-w-[400px] w-full mx-auto'>
         {showModal && <Modal text={modalText} showModal={showModal} />}
         <h1 className='text-center text-[28px] mb-[40px] font-normal'>
-          Login to your account
+          Confirn your new password
         </h1>
         <form className='w-full' onSubmit={formSubmit}>
-          <div className='mb-4 w-full'>
-            <label htmlFor='email' className='w-full mb-3 ml-2'>
-              Email address
-            </label>
-            <input
-              type='email'
-              name='email'
-              value={formData.value}
-              placeholder='Enter your email address'
-              onChange={handleChange}
-              className='w-full border border-brand-stroke rounded-lg p-3'
-            />
-          </div>
           <div className='w-full mb-4'>
             <label htmlFor='password' className='w-full mb-3 ml-2'>
               Password
@@ -139,50 +85,51 @@ const ResetPasswordConfirm = ({ handleClick }) => {
             <input
               type='password'
               name='password'
-              value={formData.value}
+              value={formData.password}
               placeholder='Enter password'
               onChange={handleChange}
               className='w-full border border-brand-stroke rounded-lg p-3'
             />
           </div>
-          <div className='w-full mb-4 flex gap-2 items-center'>
+          <div className='w-full mb-4'>
+            <label htmlFor='password' className='w-full mb-3 ml-2'>
+              Confirm password
+            </label>
             <input
-              type='checkbox'
-              name='remember'
-              id='remember'
-              checked={formData.remember}
+              type='password'
+              name='rePassword'
+              value={formData.rePassword}
+              placeholder='Enter password'
               onChange={handleChange}
+              className='w-full border border-brand-stroke rounded-lg p-3'
             />
-            <label htmlFor='remember'>Remember me</label>
           </div>
-          <button disabled={loading} className={`${styles.button} w-full`}>
-            {loading ? 'Please wait...' : 'Login to store'}
-          </button>
-          <div className='w-full flex justify-between items-center mt-2'>
-            <button
-              className='text-brand-gray font-normal text-[14px]'
-              type='button'
-              onClick={() => navigate("/reset-password")}
-            >
-              Forgot password?
-            </button>
 
+          <button disabled={loading} className={`${styles.button} w-full`}>
+            {loading ? 'Please wait...' : 'Reset password'}
+          </button>
+
+          <div className='w-full flex justify-between items-center mt-2'>
             <Link to="/signUp">
-              <button
-                className='text-brand-gray font-normal text-[14px]'
-                type='button'
-                // onClick={() => {
-                //   handleClick('signUp');
-                //   navigate('/register');
-                // }}
-              >
-                Register
-              </button>
+                <button
+                    className='text-brand-gray font-normal text-[14px]'
+                    type='button'
+                >
+                    Register
+                </button>
+            </Link>
+
+            <Link to="/login">
+                <button
+                    className='text-brand-gray font-normal text-[14px]'
+                    type='button'
+                >
+                    Login
+                </button>
             </Link>
           </div>
         </form>
       </div>
-
     </div>
   );
 };
